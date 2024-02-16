3bb57361e7ecf7bf4f9c7567608ac6b14452cddd
Raphaël Ligier <me@whiteshoulders.fr>
2024-02-16 17:19:49 +0100

    test

--- build.js
+++ build.js
@@ -1,7 +1,21 @@
 import * as frugal from "frugal";
 import config from "./frugal.config.js";
+import * as util from '@vercel/routing-utils'
 
-await frugal.build(config);
+const { routes } = util.getTransformedRoutes({
+    redirects: [
+      { source: '/me', destination: '/profile.html' },
+      { source: '/view-source', destination: 'https://github.com/vercel/vercel' },
+    ],
+    rewrites: [{
+        source: "/(.*)",
+        destination: "/"
+    }]
+  });
+
+console.log(routes)
+
+//await frugal.build(config);
 
 // zozo
 //const context = await frugal.context(config);
